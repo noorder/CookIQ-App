356f1ccbe086dc6e40c6746b5b43ff1661c0b8b8
Stas Chekalaev <schekalaev@gmail.com>
2022-02-21 01:59:19 +0300

    1

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -16,8 +16,8 @@ class AdminController extends Controller
 
     public function loginAction()
     {
-        if(isset($_SESSION['admin'])){
-            
+        if (isset($_SESSION['admin'])) {
+
             $this->view->redirect('admin/add');
         }
         if (!empty($_POST)) {
@@ -32,23 +32,39 @@ class AdminController extends Controller
 
     public function addAction()
     {
+        if (!empty($_POST)) {
+            if (!$this->model->postValidate($_POST, 'add')) {
+                $this->view->message('ERROR', $this->model->error);
+            }
+            $this->view->message('success', 'OK');
+        }
         $this->view->render('Добавить пост');
     }
+
+    public function editAction()
+    {
+        if (!empty($_POST)) {
+            if (!$this->model->postValidate($_POST, 'edit')) {
+                $this->view->message('ERROR', $this->model->error);
+            }
+            $this->view->message('success', 'OK');
+        }
+        $this->view->render('Редактирование поста');
+    }
+
+
 
     public function deleteAction()
     {
         $this->view->render('Удаление поста');
     }
 
-    public function editAction()
-    {
-        $this->view->render('Редактирование поста');
-    }
+
 
     public function logoutAction()
     {
         unset($_SESSION['admin']);
-        $this->view->redirect('admin/login');        
+        $this->view->redirect('admin/login');
     }
 
     public function postsAction()
@@ -56,3 +72,6 @@ class AdminController extends Controller
         $this->view->render('Посты');
     }
 }
+
+
+///////////#4 00
--- application/models/Admin.php
+++ application/models/Admin.php
@@ -19,4 +19,28 @@ class Admin extends Model
         }
         return true;
     }
+
+    public function postValidate($post, $type)
+    {
+
+        $nameLen = iconv_strlen($post['name']);
+        $descriptionLen = iconv_strlen($post['description']);
+        $textLen = iconv_strlen($post['text']);
+        
+        if ($nameLen < 3 or  $nameLen > 20) {
+            $this->error = 'Ошибка названия(3-20)';
+            return false;
+        } elseif ($descriptionLen < 3 or  $descriptionLen > 20) {
+            $this->error = 'Ошибка описания(3-20)';
+            return false;
+        } elseif ($textLen < 3 or  $textLen > 2000) {
+            $this->error = 'Ошибка текста(3-2000)';
+            return false;
+        }
+        if (empty($_FILES['img']['tmp_name']) and $type == 'add') {
+            $this->error = 'Ошибка фото';
+            return false;
+        }
+        return true;
+    }
 }
